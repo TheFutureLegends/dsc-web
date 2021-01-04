5be6ab78aae42e0a899fe2a0d5911b49acb58d85
Tri Nguyen <leonguyen0202@gmail.com>
2021-01-04 19:52:59 +0700

    [fix]: Remove un-necessary package

--- src/routes.js
+++ src/routes.js
@@ -1,53 +1,8 @@
-/*!
-
-=========================================================
-* Black Dashboard PRO React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-pro-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-// import VectorMap from "views/maps/VectorMap.js";
-// import GoogleMaps from "views/maps/GoogleMaps.js";
-// import FullScreenMap from "views/maps/FullScreenMap.js";
-
-// import RegularTables from "views/tables/RegularTables.js";
-// import ExtendedTables from "views/tables/ExtendedTables.js";
-// import Wizard from "views/forms/Wizard.js";
-// import ValidationForms from "views/forms/ValidationForms.js";
-// import ExtendedForms from "views/forms/ExtendedForms.js";
-// import RegularForms from "views/forms/RegularForms.js";
-
-// import Buttons from "views/components/Buttons.js";
-
-// import Notifications from "views/components/Notifications.js";
-// import Grid from "views/components/Grid.js";
-// import Typography from "views/components/Typography.js";
-
-// import Icons from "views/components/Icons.js";
-// import Pricing from "views/pages/Pricing.js";
-// import Register from "views/pages/Register.js";
-// import Timeline from "views/pages/Timeline.js";
-// import User from "views/pages/User.js";
-
-// import Rtl from "views/pages/Rtl.js";
-// import Lock from "views/pages/Lock.js";
-
-// import ReactTables from "./views/tables/ReactTables.js";
-// import Calendar from "./views/Calendar.js";
-// import Widgets from "./views/Widgets.js";
-// import Charts from "./views/Charts.js";
+/**
+ * Main page for users
+ */
 import Homepage from "./views/homepage/Homepage.js";
 import PostDetailPage from "./views/postDetail/PostDetail.js";
-
-// import SweetAlert from "./views/components/SweetAlert.js";
 
 /**
  * Authentication Page
@@ -114,28 +69,6 @@ export const protectedRoutes = [
     // redirect: "/auth/login",
     invisible: true,
   },
-
-  /**
-   * Sweet Alert Test
-   */
-  // {
-  //   collapse: true,
-  //   name: "Components",
-  //   rtlName: "المكونات",
-  //   icon: "tim-icons icon-molecule-40",
-  //   state: "componentsCollapse",
-  //   views: [
-  //     {
-  //       path: "/sweet-alert",
-  //       name: "Sweet Alert",
-  //       rtlName: "الحلو تنبيه",
-  //       mini: "SA",
-  //       rtlMini: "ومن",
-  //       component: SweetAlert,
-  //       layout: "/control-panel",
-  //     },
-  //   ],
-  // },
 ];
 
 const routes = [
